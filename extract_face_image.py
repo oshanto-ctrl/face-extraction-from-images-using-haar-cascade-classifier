a99173bc7658fb2b96a45deb76d35f812f68bac8
Md. Rejoan Siddiky <55896261+oshanto-ctrl@users.noreply.github.com>
2022-11-02 16:59:30 +0600

    Update extract_face_image.py

--- extract_face_image.py
+++ extract_face_image.py
@@ -4,8 +4,8 @@ import glob
 # haarcascades face extraction model
 face_cascade = cv2.CascadeClassifier("C:\\Users\\USER\\AppData\\Local\\Programs\\Python\\Python310\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml")
 
-# file path
-path = "E:\\#Research_Info\\video_dataset_folder_male\\image_dataset_two\\*.*"
+# file path of image datasets
+path = "E:\\#Research_Info\\video_dataset_folder_male\\image_dataset_folder\\*.*"
 
 image_number = 1
 
@@ -21,10 +21,21 @@ for file in image_list:
         for (x, y, w, h) in face_images:
             region_of_interest = image[y:y+h, x:x+w]
         resized = cv2.resize(region_of_interest, (128, 128))
-        cv2.imwrite("E:\\#Research_Info\\video_dataset_folder_male\\extracted_faces_dataset_two/" + str(image_number) + ".png", resized)
+        cv2.imwrite("E:\\#Research_Info\\video_dataset_folder_male\\extracted_faces_dataset_folder/" + str(image_number) + ".png", resized)
     except:
-        print("Detected No Faces.")
+        print("No Faces Detected.")
     
     image_number += 1
 
 print("Done Interpreting the code.")
+
+
+
+
+
+
+
+
+
+
+
