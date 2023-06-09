2b040ec66575ac6f6e58c09cab7379d2f14ecb0b
Md. Rejoan Siddiky <55896261+oshanto-ctrl@users.noreply.github.com>
2023-06-09 23:42:57 +0600

    Update raname_pictures.py
    Added comments and spaces.

--- raname_pictures.py
+++ raname_pictures.py
@@ -1,15 +1,20 @@
 # this script renames images from a specific file
+# import necessary modules
 import os
 
+# in file_path variable you set your own image dataset folder path
 file_path = os.chdir("E:\\#Research_Info\\video_dataset_folder_male\\image_dataset_folder")
 
-i = 1
+i = 1 # starting from image no. 1 (example: 1.png)
 
 for image_file in os.listdir(file_path):
     renamed_file = f"{i}.png"
     os.rename(image_file, renamed_file)
     i += 1
     # print(f"Renamed {i}'th image: Ok")
+  
+# Completion
+print("Renaming Complete.")
 
     
     
